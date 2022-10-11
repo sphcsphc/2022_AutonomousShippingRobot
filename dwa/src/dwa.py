f0bbe83b28c5ef63eeb0ac4772fe9d63a5f90f17
sphcsphc <mataph132@gmail.com>
2022-10-11 15:58:13 +0900

    Update DWA

--- dwa/src/dwa.py
+++ dwa/src/dwa.py
@@ -4,7 +4,6 @@
 import numpy as np
 import rospy
 import tf
-import open3d as o3d
 from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import Twist, Pose, Pose2D
 from open3d_ros_helper import open3d_ros_helper as orh
@@ -13,108 +12,115 @@ from open3d_ros_helper import open3d_ros_helper as orh
 def main():
     rospy.init_node('dwa_test')
     publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-    driver = SelfDrive(publisher)
-    driver.calculation()
-    while not rospy.is_shutdown():
-        rospy.Subscriber('velodyne_points', PointCloud2, driver.lds_callback)
-        rospy.spin()
+    driver = DWA(publisher)
+    rospy.Subscriber('velodyne_points', PointCloud2, driver.lds_callback)
+    rospy.spin()
 
 
-class SelfDrive:
+class DWA:
     def __init__(self, publisher):
         rospy.Subscriber('current_pose', Pose, self.current_pose)
         self.publisher = publisher
-        self.global_pose = Pose2D()
-        self.lidar_array = np.array([])
-        self.mps = [0.2, 0.17, 0.15, 0.13, 0.1]
-        self.radps = [0, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, -0.3, -0.5, -0.6, -0.7, -0.8, -0.9]
-        self.t_array = 0.25 * np.arange(1, 11).reshape(10, 1, 1)
-        self.best_score = None
-        self.radps_array = None
-        self.motion = None
-        self.local_pos = None
-        self.start_x = -4.115
-        self.start_y = 0.409
-        self.goal_x = -1.633
-        self.goal_y = 0.317
-        self.save_x = -1.979
-        self.save_y = 0.0
-
-    def current_pose(self, data):
-        self.global_pose.x = data.position.x
-        self.global_pose.y = data.position.y
-        _, _, self.global_pose.theta = tf.transformations.euler_from_quaternion([data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w])
+        self.scout_pose = Pose2D()
+        self.dwa_mode = "patrol"
+        self.home_pos = 8.14, 2.14
+        self.goal_pos = 8.4483, 1.7833
 
     def lds_callback(self, velo):
         turtle_vel = Twist()
+        mps = [0.1, 0.12, 0.14, 0.16, 0.18, 0.2, 0.22, 0.24, 0.26, 0.28]
+        radps = [0, 0.2, 0.4, 0.6, 0.8, -0.2, -0.4, -0.6, -0.8, -0.9]
         data = orh.rospc_to_o3dpc(velo)
-        self.preprocessing(data)
-        self.remaining_scoring()
-        turtle_vel.linear.x, turtle_vel.angular.z = self.obstacle_scoring()
-        if np.hypot(self.goal_x - self.global_pose.x, self.goal_y - self.global_pose.y) <= 0.10:
-            rospy.loginfo_once("done")
+        data_array = self.preprocessing(data)
+        local_pos = self.make_combination(mps, radps)
+        pos_candidates = self.create_pos_candidates(local_pos)
+        if self.dwa_mode == "patrol":
+            goal_pos = self.goal_pos
+        elif self.dwa_mode == "home":
+            goal_pos = self.home_pos
+        if np.hypot(goal_pos[0] - self.scout_pose.x, goal_pos[1] - self.scout_pose.y) <= 0.8:
             turtle_vel.linear.x, turtle_vel.angular.z = 0., 0.
+        else:
+            best_score, back_check = self.evaluate_scores(pos_candidates, goal_pos, data_array, local_pos)
+            turtle_vel.linear.x, turtle_vel.angular.z = mps[best_score[0]], radps[best_score[1]]
+            if back_check:
+                turtle_vel.linear.x = mps[best_score[0]] * -1
+        rospy.loginfo("{} {}".format(turtle_vel.linear.x, turtle_vel.angular.z))
         self.publisher.publish(turtle_vel)
 
-    def preprocessing(self, data):
-        self.data_array = np.asarray(data.points)
-        self.data_array = self.data_array[
-            np.logical_and(np.logical_or(self.data_array[:, 0] > -0.45, self.data_array[:, 0] < -0.47),
-                           np.abs(self.data_array[:, 1]) > 0.17)]
-        self.data_array = self.data_array[np.logical_and(self.data_array[:, 2] > -0.55, self.data_array[:, 2] < 0.5)]
+    def current_pose(self, data):
+            self.scout_pose.x = data.position.x
+            self.scout_pose.y = data.position.y
+            _, _, self.scout_pose.theta = tf.transformations.euler_from_quaternion(
+                [data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w])
+    
+    @staticmethod
+    def preprocessing(data):
+        data_array = np.asarray(data.points)
+        data_array = data_array[np.logical_and(np.logical_or(data_array[:, 0] > -0.45, data_array[:, 0] < -0.47),
+                                               np.abs(data_array[:, 1]) > 0.17)]
+        data_array = data_array[np.logical_and(data_array[:, 2] > -0.55, data_array[:, 2] < 0.5)]
+        return data_array
 
-    def calculation(self):
-        mps_array = np.array(self.mps).reshape(len(self.mps), 1)
-        self.radps_array = np.delete(np.array(self.radps), 0)
-        line_motion = mps_array * self.t_array
-        rotational_motion = 2 * (mps_array / self.radps_array ) * np.sin(0.5 * self.radps_array * self.t_array) + 0.05
-        self.motion = np.concatenate((line_motion, rotational_motion), axis=2) + 0.4
-        local_x_pos = np.concatenate((line_motion, rotational_motion * np.cos(0.5 * self.radps_array * self.t_array)), axis=2)
-        local_y_pos = np.concatenate((np.zeros((10, len(self.mps), 1)), rotational_motion * np.sin(0.5 * self.radps_array * self.t_array)), axis=2)
-        self.local_pos = np.concatenate((np.reshape((local_x_pos), (10, -1, 1)), np.reshape((local_y_pos), (10, -1, 1))), axis=2)
+    @staticmethod
+    def make_combination(mps, radps):
+        line_motions = np.array(mps).reshape(len(mps), 1)
+        radps_array = np.delete(np.array(radps), 0)
+        rotational_motions = 2 * (line_motions / radps_array) * np.sin(0.5 * radps_array) + 0.05
+        local_x_pos = np.concatenate((line_motions, rotational_motions * np.cos(0.5 * radps_array)), axis=1)
+        local_y_pos = np.concatenate(
+            (np.zeros((len(mps), 1)), rotational_motions * np.sin(0.5 * radps_array)), axis=1)
+        local_pos = np.concatenate((np.reshape(local_x_pos, (-1, 1)), np.reshape(local_y_pos, (-1, 1))), axis=1)
+        return local_pos
 
-    def remaining_scoring(self):
+    def create_pos_candidates(self, local_pos):
         rotation_matrix = np.array(
-            [[np.cos(self.global_pose.theta), -np.sin(self.global_pose.theta)],
-             [np.sin(self.global_pose.theta), np.cos(self.global_pose.theta)]])
-        local_matrix = np.round_((np.dot(self.local_pos, rotation_matrix.T)), 4)
-        remain_x = self.goal_x - (self.global_pose.x + np.delete(local_matrix, 1, axis=2))
-        remain_y = self.goal_y - (self.global_pose.y + np.delete(local_matrix, 0, axis=2))
-        score = np.reshape(np.hypot(remain_x, remain_y), (10, len(self.mps), len(self.radps)))
-        self.best_score = np.unravel_index(np.argmin(score[3], axis=None), score[3].shape)
+            [[np.cos(self.scout_pose.theta), -np.sin(self.scout_pose.theta)],
+             [np.sin(self.scout_pose.theta), np.cos(self.scout_pose.theta)]])
+        rotation_trans = np.round_((np.dot(local_pos, rotation_matrix.T)), 4)
+        global_x_pos = self.scout_pose.x + np.delete(rotation_trans, 1, axis=1)
+        global_y_pos = self.scout_pose.y + np.delete(rotation_trans, 0, axis=1)
+        global_pos = np.concatenate((global_x_pos, global_y_pos), axis=1)
+        return global_pos
 
-    def obstacle_scoring(self):
-        back = np.logical_and(np.logical_and(self.data_array[:, 0] >= 0.3, self.data_array[:, 0] < 0.55),
-                              np.logical_and(self.data_array[:, 1] > -0.44, self.data_array[:, 1] < 0.43))
-        slow = np.logical_and(np.logical_and(self.data_array[:, 0] >= 0.55, self.data_array[:, 0] < 0.8),
-                              np.logical_and(self.data_array[:, 1] > -0.42, self.data_array[:, 1] < 0.41))
-        side_left = np.logical_and(
-            np.logical_and(
-                np.logical_and(self.data_array[:, 0] > -0.45, self.data_array[:, 0] < 0.),
-                np.logical_and(self.data_array[:, 1] >= 0.4, self.data_array[:, 1] < 0.68)),
-            np.logical_and(self.data_array[:, 2] >= -0.55, self.data_array[:, 2] < 0.2))
-        side_right = np.logical_and(
-            np.logical_and(
-                np.logical_and(self.data_array[:, 0] > -0.45, self.data_array[:, 0] < 0.),
-                np.logical_and(self.data_array[:, 1] > -0.69, self.data_array[:, 1] <= -0.4)),
-            np.logical_and(self.data_array[:, 2] >= -0.55, self.data_array[:, 2] < 0.2))
-        if True in back:
-            rospy.loginfo("back")
-            return -0.2, self.radps[self.best_score[1]]
-        elif True in slow:
-            rospy.loginfo("slow")
-            return 0.15, self.radps[self.best_score[1]]
-        elif True in side_left and True in side_right:
-            rospy.loginfo("side back")
-            return -0.2, 0.
-        elif True in side_left: # 우회전
-            rospy.loginfo("side left")
-            return self.mps[self.best_score[0]], -0.15
-        elif True in side_right: # 좌회전
-            rospy.loginfo("side right")
-            return self.mps[self.best_score[0]], 0.15
-        rospy.loginfo("straight")
-        return self.mps[self.best_score[0]], self.radps[self.best_score[1]]
+    def evaluate_scores(self, pos_candidates, goal_pos, data_array, local_pos):
+        remaining_scores = self.find_remaining_scores(pos_candidates, goal_pos)
+        obstacle_distance, obstacle_scores = self.find_obstacle_scores(data_array, local_pos)
+        clearance_scores = self.find_clearance_scores(data_array, local_pos)
+        scores = 1.6 * remaining_scores + obstacle_scores + clearance_scores
+        best_score = np.unravel_index(np.argmax(scores), scores.shape)
+        return best_score, obstacle_distance[best_score] <= 0.43
+
+    @staticmethod
+    def find_remaining_scores(pos_candidates, goal_pos):
+        x = goal_pos[0] - np.delete(pos_candidates, 1, axis=1)
+        y = goal_pos[1] - np.delete(pos_candidates, 0, axis=1)
+        scores = 1 / np.reshape(np.hypot(x, y), (10, 10))
+        norm = np.linalg.norm(scores)
+        scores = scores / norm
+        return scores
+
+    @staticmethod
+    def find_obstacle_scores(data_array, local_pos):
+        x = data_array[:, 0] - np.delete(local_pos, 1, axis=1)
+        y = data_array[:, 1] - np.delete(local_pos, 0, axis=1)
+        distance = np.reshape(np.amin(np.hypot(x, y), axis=1), (10, 10))
+        scores = np.where(distance <= 0.43, 0, distance)
+        if np.any(scores) != 0:
+            norm = np.linalg.norm(scores)
+            scores = scores / norm
+        return distance, scores
+
+    @staticmethod
+    def find_clearance_scores(data_array, local_pos):
+        data_array = data_array[np.logical_and(np.logical_and(data_array[:, 0] > -0.1, data_array[:, 0] < 1.0),
+                                               np.abs(data_array[:, 1]) > 1.0)]
+        x = data_array[:, 0] - np.delete(local_pos, 1, axis=1)
+        y = data_array[:, 1] - np.delete(local_pos, 0, axis=1)
+        scores = np.reshape(np.mean(np.hypot(x, y), axis=1), (10, 10))
+        norm = np.linalg.norm(scores)
+        scores = scores / norm
+        return scores
 
 
 if __name__ == '__main__':
